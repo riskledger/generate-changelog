88f945436e421e262cbc81b49c7706e473e06199
Dimitris Karittevlis <dimitris@riskledger.com>
2020-10-29 16:43:50 +0000

    remove unused

--- main.js
+++ main.js
@@ -1,22 +1,10 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
 const { Changelog } = require('lerna-changelog');
 
 const tagFrom = core.getInput('from', { required: true });
 const tagTo = core.getInput('to', { required: true });
 
 const cl = new Changelog();
-const changelog = cl.createMarkdown({ tagFrom: 't', tagTo: 'HEAD' });
+const changelog = cl.createMarkdown({ tagFrom, tagTo });
 
 core.setOutput('changelog', changelog);
-
-// console.log(changelog);
-
-// exec
-//   .exec(`lerna-changelog --from ${tagFrom} --to ${tagTo}`)
-//   .then((changelog) => {
-//     core.setOutput('changelog', changelog);
-//   })
-//   .catch((err) => {
-//     core.setFailed(`Action failed with error ${err}`);
-//   });
