e3e74469b1d07183b8e25a935299009870c93fb7
Dimitris Karittevlis <dimitris@riskledger.com>
2020-10-29 16:36:25 +0000

    use then instead of await

--- main.js
+++ main.js
@@ -5,9 +5,20 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
+// const { Changelog } = require('lerna-changelog');
+
 const tagFrom = core.getInput('from', { required: true });
 const tagTo = core.getInput('to', { required: true });
 
-const changelog = await exec.exec(`lerna-changelog --from ${tagFrom} --to ${tagTo}`);
+// const changelog = new Changelog({ tagFrom: 't', tagTo: 'HEAD' });
 
-core.setOutput('changelog', changelog);
+// console.log(changelog);
+
+exec
+  .exec(`lerna-changelog --from ${tagFrom} --to ${tagTo}`)
+  .then((changelog) => {
+    core.setOutput('changelog', changelog);
+  })
+  .catch((err) => {
+    core.setFailed(`Action failed with error ${err}`);
+  });
