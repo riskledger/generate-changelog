47192f85dbb5722d74cc92c2b2533a65dc183482
Dimitris Karittevlis <dimitris@riskledger.com>
2020-10-29 17:39:49 +0000

    log github context

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const { Changelog } = require('lerna-changelog');
 const { load } = require('lerna-changelog/lib/configuration');
+
+const context = github.context;
+console.log(context);
 
 const tagFrom = core.getInput('from', { required: true });
 const tagTo = core.getInput('to', { required: true });
