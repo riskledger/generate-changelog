1e900367f6814c1ace87ca5cb25705dbc5bf5f49
Dimitris Karittevlis <dimitris@riskledger.com>
2020-10-29 16:41:51 +0000

    use lerna programmaticaly

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-// How to install? cache?
-// const cp = require("child_process");
-// cp.execSync(`cd ${__dirname}; npm ci`);
-
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-
-// const { Changelog } = require('lerna-changelog');
+const { Changelog } = require('lerna-changelog');
 
 const tagFrom = core.getInput('from', { required: true });
 const tagTo = core.getInput('to', { required: true });
 
-// const changelog = new Changelog({ tagFrom: 't', tagTo: 'HEAD' });
+const cl = new Changelog();
+const changelog = cl.createMarkdown({ tagFrom: 't', tagTo: 'HEAD' });
+
+core.setOutput('changelog', changelog);
 
 // console.log(changelog);
 
-exec
-  .exec(`lerna-changelog --from ${tagFrom} --to ${tagTo}`)
-  .then((changelog) => {
-    core.setOutput('changelog', changelog);
-  })
-  .catch((err) => {
-    core.setFailed(`Action failed with error ${err}`);
-  });
+// exec
+//   .exec(`lerna-changelog --from ${tagFrom} --to ${tagTo}`)
+//   .then((changelog) => {
+//     core.setOutput('changelog', changelog);
+//   })
+//   .catch((err) => {
+//     core.setFailed(`Action failed with error ${err}`);
+//   });
