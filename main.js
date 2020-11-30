3d44efcfb88c70b4d8793091340febf9c579c54e
Dimitris Karittevlis <safinn@users.noreply.github.com>
2020-11-30 11:40:20 +0000

    use head sha

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const github = require('@actions/github');
 const { Changelog } = require('lerna-changelog');
 const { load } = require('lerna-changelog/lib/configuration');
 
+const tagTo = github.context.payload.pull_request.head.sha;
 const tagFrom = github.context.payload.pull_request.base.sha;
-const tagTo = github.context.sha;
 
 const config = load({ nextVersionFromMetadata: false });
 
