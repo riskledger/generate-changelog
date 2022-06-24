ecd75d45ea55386d9c2b47cd6499852a1383ab6c
Daniel McKenzie <16889045+McKenzieJDan@users.noreply.github.com>
2022-06-24 16:48:08 +0100

    Refactor to support on push

--- main.js
+++ main.js
@@ -3,13 +3,42 @@ const github = require('@actions/github');
 const { Changelog } = require('lerna-changelog');
 const { load } = require('lerna-changelog/lib/configuration');
 
-const tagTo = github.context.payload.pull_request.head.sha;
-const tagFrom = github.context.payload.pull_request.base.sha;
+async function run() {
+  try {
+    const token = core.getInput('token', { required: true });
+    const title = core.getInput('title', { required: true });
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    
+    const octokit = github.getOctokit(token);
 
-const config = load({ nextVersionFromMetadata: false });
+    const { data: pullRequest } = await octokit.rest.pulls.list({
+      owner: owner,
+      repo: repo,
+    })
 
-const cl = new Changelog(config);
+    var result = pullRequest.filter(obj => {
+      return obj.title === title
+    })
 
-cl.createMarkdown({ tagFrom, tagTo })
-  .then((changelog) => core.setOutput('changelog', changelog))
-  .catch((err) => core.warning(`Failed generating changelog ${err}`));
+    console.log(result[0].head.sha)
+    console.log(result[0].base.sha)
+
+    // current
+    const tagTo = result[0].head.sha;
+    const tagFrom = result[0].base.sha;
+
+    const config = load({ nextVersionFromMetadata: false });
+
+    const cl = new Changelog(config);
+
+    cl.createMarkdown({ tagFrom, tagTo })
+      .then((changelog) => core.setOutput('changelog', changelog))
+      .catch((err) => core.warning(`Failed generating changelog ${err}`));
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+run();
